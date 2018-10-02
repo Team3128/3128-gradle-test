0fb3163414e166f9ee49d55b6140dbd0d4c16b75
googolplex8 <googolplex8@yahoo.com>
2018-10-01 19:58:19 -0700

    annotating lesson 1

--- src/main/java/org/team3128/lessons/Lesson1.java
+++ src/main/java/org/team3128/lessons/Lesson1.java
@@ -16,8 +16,6 @@ import org.team3128.common.listener.controllers.ControllerExtreme3D;
 import org.team3128.common.listener.controltypes.Button;
 import edu.wpi.first.wpilibj.Joystick;
 
-
-
 public class Lesson1 extends NarwhalRobot {
     //two VictorSPX motor controllers
     VictorSPX victor1;
@@ -37,8 +35,10 @@ public class Lesson1 extends NarwhalRobot {
     double track = 0.0*Length.in;
     int robotFreeSpeed = 0; //native units per 100ms, a native unit is 1/4096 of a rotation
 
+    //listens for something to change on joystick (joystick pushed, button pressed, etc)
 	public ListenerManager lm;
 
+    //what we control the robot with
 	public Joystick joystick;
 
 
@@ -47,25 +47,44 @@ public class Lesson1 extends NarwhalRobot {
         //this is how you print something to the console on the Driver Station
         Log.info("robot", "constructing harware for the robot!");
 
+        //initialization of two VictorSPX motors controller
         victor1 = new VictorSPX(0);
         victor2 = new VictorSPX(1);
-        victor1.set(ControlMode.PercentOutput, 0);
-        victor2.set(ControlMode.Current, 0);
 
+        /**There are many ways to tell a motor what to do using 
+         * PercentOutput: Explanation here
+         * Position: Explanation here
+         * Velocity: Explanation here
+         * Current: Explanation here
+         * Follower: Explanation here
+         * MotionProfile: Explanation here
+         * MotionMagic: Explanation here
+         * MotionProfileArc: Explanation here
+         * **/
+
+        //set motor to 10% of max output
+        victor1.set(ControlMode.PercentOutput, 10);
+        //set motor to a velocity of 0
+        victor2.set(ControlMode.Velocity, 0);
+
+        //initialization of two TalonSRX motor controllers
         right1 = new TalonSRX(2);
         right2 = new TalonSRX(3);
-        //right1.set(ControlMode.Position, 0);
-        //right1.set(ControlMode.Velocity, 0);
+ 
+        //lets motors controller know that they will be hooked up to encoder (sensor)
         right1.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, Constants.CAN_TIMEOUT);
         left1.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, Constants.CAN_TIMEOUT);
         
         right2.set(ControlMode.Follower, right1.getDeviceID());
         left2.set(ControlMode.Follower, left1.getDeviceID());
 
+        //initilization of SRXTankDrive object.
+        //hover over the word "initialize" on the line below to learn more about the params
         SRXTankDrive.initialize(left1, left2, wheelCirc, gearRatio, wheelBase, track,
 				robotFreeSpeed);
         drive = SRXTankDrive.getInstance();
         
+        //initialization of joystick and listener manager
         joystick = new Joystick(1);
 		lm = new ListenerManager(joystick);
 		addListenerManager(lm);
@@ -73,9 +92,16 @@ public class Lesson1 extends NarwhalRobot {
 
     @Override
     protected void setupListeners() {
+        //designating what each of the parts of the joystick should be called
+        //aka the value of the joystick when it is pushed in the y-direction is called "MoveForwards"
         lm.nameControl(ControllerExtreme3D.JOYY, "MoveForwards");
 		lm.nameControl(ControllerExtreme3D.TWIST, "MoveTurn");
 		lm.nameControl(ControllerExtreme3D.THROTTLE, "Throttle");
+
+        //below you will see lambas i.e. the ()->{} part of the code
+        //a lambda essentially pases in part of a function as a parameter rather than a single value
+        //I will explain this more somewhere else... or click on the website below for more info
+        //https://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html
 
 		lm.addMultiListener(() ->
 		{
